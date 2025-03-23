68c6808d6a76a302e6890770129d62f4c1ff5323
cyqhanser985 <2660920607@qq.com>
2025-03-23 20:51:53 +0800

    Update test2.cpp

--- 前缀和/partial_sum学习/test2.cpp
+++ 前缀和/partial_sum学习/test2.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 int main(){
     vector<vector<int>> nums={{1,2},{3,4},{5,6}}
-    vector<vector<int>> prefix(nums.size(),vector<int>(nums[0].size(),0));
+    vector<vector<int>> prefix(nums.size());
 
     for(int i=0;i<nums.size();i++){
-        partial_sum(nums[i].begin(),nums[i].end(),prefix[i].begin());
+        partial_sum(nums[i].begin(),nums[i].end(),back_inserter(prefix[i]));
     }
 
     for(int i=0;i<nums.size();i++){
